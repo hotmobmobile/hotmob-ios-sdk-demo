9f3e190c336b354dc675923a72f4a6720c757462
Chiu Choi <chiu.choi@hot-mob.com>
2016-01-26 16:38:28 +0800

    pageview demo

---
+++ Example/SampleProject2/SampleProject2/ViewController/TestTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TestTableViewController.h
+//  SampleProject2
+//
+//  Created by ChoiWing Chiu on 25/1/2016.
+//  Copyright © 2016 Hotmob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewController : UITableViewController
+
+
+- (void) reloadBanner;
+@end
---
+++ Example/SampleProject2/SampleProject2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleProject2
+//
+//  Created by ChoiWing Chiu on 25/1/2016.
+//  Copyright © 2016 Hotmob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
