cddc3c0ad7fbdbb041b07a60630e63561c66faa5
Ken Wong <ken.wong@hot-mob.com>
2019-08-29 17:28:40 +0800

    - upgrade to 4.6.3

--- HotmobSDK/HotmobManager.h
+++ HotmobSDK/HotmobManager.h
@@ -5,8 +5,8 @@
 //  Created by Hotmob Ltd. on 23/7/15.
 //  Copyright (c) 2015 Hotmob Ltd. All rights reserved.
 //
-// version 4.6.2 55LR3
-// update at 2018/11/01
+// version 4.6.3 ZF6UB
+// update at 2019/08/29
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
